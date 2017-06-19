3ac8136ca39feb438f394a2bb1128e120c0abc8e
Luis Montealegre <montealegreluis@gmail.com>
2017-06-19 11:46:02 -0500

    Ad methods for the first CRUD operations on Posts
    * Added methods to view all, view one, and save a post.

--- src/main/java/com/codeup/controllers/PostsController.java
+++ src/main/java/com/codeup/controllers/PostsController.java
@@ -3,5 +3,35 @@
  */
 package com.codeup.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostsController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String viewAll() {
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String viewIndividualPost(@PathVariable long id) {
+        return "view an individual post with ID " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String showPostForm() {
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String savePost() {
+        return "create a new post";
+    }
 }
