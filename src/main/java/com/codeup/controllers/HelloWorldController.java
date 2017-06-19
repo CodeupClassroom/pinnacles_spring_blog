59fd2ee7bb82add3db5d33f251b02f3d3fe30536
Luis Montealegre <montealegreluis@gmail.com>
2017-06-19 09:51:39 -0500

    Create examples using the different Spring annotations
    * Added examples using one and several path variables
    * Added example using RequestMapping instead of GetMapping

--- src/main/java/com/codeup/controllers/HelloWorldController.java
+++ src/main/java/com/codeup/controllers/HelloWorldController.java
@@ -3,5 +3,31 @@
  */
 package com.codeup.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller  // @WebServlet
 public class HelloWorldController {
+
+    @GetMapping("/hello")  //( urlPatterns)
+    @ResponseBody          // response.getWriter.println("hello world!")
+    public String hello() {
+        return "Hello world!";
+    }
+
+    @GetMapping("/hello/{name}/{lastName}") // shortcut for RequestMapping (method = GET)
+    @ResponseBody
+    public String helloFriend(@PathVariable String name, @PathVariable String lastName) {
+        return String.format("Hello %s %s!", name, lastName);
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        // if, switch, while, do-while, for
+        return number + " plus one is " + (number + 1) + "!";
+    }
 }
+
+
+
