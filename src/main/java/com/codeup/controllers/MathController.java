78870b7cf441c08c664c36c754b455c8a9d63f29
Luis Montealegre <montealegreluis@gmail.com>
2017-06-19 11:36:32 -0500

    Add basic arithmetic operations to MathController
    * divide method returns a double instead of an int

--- src/main/java/com/codeup/controllers/MathController.java
+++ src/main/java/com/codeup/controllers/MathController.java
@@ -3,5 +3,34 @@
  */
 package com.codeup.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MathController {
+    @GetMapping("/add/{number1}/and/{number2}")
+    @ResponseBody
+    public int add(@PathVariable int number1, @PathVariable int number2) {
+        return number1 + number2;
+    }
+
+    @GetMapping("/subtract/{number1}/from/{number2}")
+    @ResponseBody
+    public int subtract(@PathVariable int number1, @PathVariable int number2) {
+        return number1 - number2;
+    }
+
+    @GetMapping("/multiply/{number1}/and/{number2}")
+    @ResponseBody
+    public int multiply(@PathVariable int number1, @PathVariable int number2) {
+        return number1 * number2;
+    }
+
+    @GetMapping("/divide/{number1}/by/{number2}")
+    @ResponseBody
+    public double divide(@PathVariable int number1, @PathVariable int number2) {
+        return (double)  number1 / number2;
+    }
 }
