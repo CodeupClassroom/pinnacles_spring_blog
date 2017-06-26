e58aef2ceafb08e0b6404a277b0e206f2145144f
Luis Montealegre <montealegreluis@gmail.com>
2017-06-26 09:40:54 -0500

    Add annotations to validate title and description
    * Both title and description cannot be empty
    * The title minimum length is 5 characters

--- src/main/java/com/codeup/models/Ad.java
+++ src/main/java/com/codeup/models/Ad.java
@@ -1,6 +1,9 @@
 package com.codeup.models;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
@@ -12,9 +15,12 @@ public class Ad {
     private long id;
 
     @Column(nullable = false, length = 100)
+    @NotBlank(message = "The title of your ad cannot be empty")
+    @Size(min = 5, message = "Titles must be at least 3 characters long")
     private String title;
 
     @Column(nullable = false, columnDefinition = "Text")
+    @NotBlank(message = "Please provide a description for your ad")
     private String description;
 
     @ManyToOne
