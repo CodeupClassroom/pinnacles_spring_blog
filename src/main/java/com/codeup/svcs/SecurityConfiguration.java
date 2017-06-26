209833457af02d218d158af575927f28d66b0924
Fernando <fernandoza@gmail.com>
2017-06-26 11:32:59 -0500

    Several front end changes for a better look.

--- src/main/java/com/codeup/svcs/SecurityConfiguration.java
+++ src/main/java/com/codeup/svcs/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.codeup.svcs;
 
-import com.codeup.repositories.UserDetailsLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
